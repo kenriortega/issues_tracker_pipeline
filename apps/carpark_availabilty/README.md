3f7e3da9f486364fc4dca8e66917fa2a24b7bbfa
kenriortega <kenriortega@gmail.com>
2022-09-24 10:37:00 -0400

    feat: add carpark availability starter project

---
+++ apps/carpark_availabilty/README.md
@@ -0,0 +1,6 @@
+# Carparck Availabilty in Singapure Notifications systems
+
+
+### Resource
+
+[https://data.gov.sg/dataset/carpark-availability](https://data.gov.sg/dataset/carpark-availability)
---
+++ apps/carpark_availabilty/config.py
@@ -0,0 +1,4 @@
+config = {
+    "url_carpark_availability": "https://api.data.gov.sg/v1/transport/carpark-availability",
+    "url_carpark_info": "https://data.gov.sg/dataset/hdb-carpark-information",
+}
