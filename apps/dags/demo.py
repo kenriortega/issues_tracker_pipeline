ef9b502fdae0b5370200e2ac6b65a39e375b6c0d
kenriortega <kenriortega@gmail.com>
2022-09-30 18:31:31 -0400

    fix: demo DAG

--- apps/dags/demo.py
+++ apps/dags/demo.py
@@ -5,14 +5,16 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 # A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2022, 9, 30), schedule="0 0 * * *") as dag:
-
+with DAG(dag_id="demo", start_date=datetime(2021, 1, 1),
+         schedule_interval="@daily", catchup=False) as dag:
     # Tasks are represented as operators
     hello = BashOperator(task_id="hello", bash_command="echo hello")
+
 
     @task()
     def airflow():
         print("airflow")
+
 
     # Set dependencies between tasks
     hello >> airflow()
