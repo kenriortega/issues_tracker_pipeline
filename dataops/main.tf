47c9d3d3629d39df54d698df97d1959d58195ed3
kenriortega <kenriortega@gmail.com>
2022-10-10 21:21:06 -0400

    ref: terraform files

--- dataops/main.tf
+++ dataops/main.tf
@@ -1,53 +1,21 @@
 
 
-# Creating namespace with the Kubernetes provider is better than auto-creation in the helm_release.
-# You can reuse the namespace and customize it with quotas and labels.
-resource "kubernetes_namespace" "playground" {
-  metadata {
-    name = var.namespace
-  }
-}
-
-resource "kubernetes_namespace" "data_serving" {
-  metadata {
-    name = "data-serving"
-  }
-}
-
-resource "kubernetes_namespace" "data_ingestion" {
-  metadata {
-    name = "data-ingestion"
-  }
-}
-resource "kubernetes_namespace" "data_processing" {
-  metadata {
-    name = "data-processing"
-  }
-}
-
-
-resource "kubernetes_namespace" "data_viz" {
-  metadata {
-    name = "data-viz"
-  }
-}
-
 
 module "airflow" {
   source    = "./modules/airflow"
-  namespace = data_processing.metadata.name
+  namespace = "data-processing"
 }
 
 module "kafka" {
   source    = "./modules/kafka"
-  namespace = data_ingestion.metadata.name
+  namespace = "data-ingestion"
 
 }
 
 
 module "ch" {
   source    = "./modules/ch"
-  namespace = data_serving.metadata.name
+  namespace = "data-serving"
 
 }
 
@@ -55,5 +23,5 @@ module "ch" {
 
 module "superset" {
   source    = "./modules/superset"
-  namespace = data_viz.metadata.name
+  namespace = "data-viz"
 }
