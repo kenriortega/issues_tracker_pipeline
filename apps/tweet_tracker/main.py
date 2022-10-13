9eb3ecc682829629ac0d7ab6e23b51ff8e070845
kenriortega <kenriortega@gmail.com>
2022-10-13 17:45:33 -0400

    ref: add filters, and query for our apache projects

--- apps/tweet_tracker/main.py
+++ apps/tweet_tracker/main.py
@@ -16,30 +16,32 @@ load_dotenv()
 bearer_token = os.getenv("BEARER_TOKEN")
 
 
-def search_tweets_by(client: tweepy.Client, arg: str):
+def extract_recent_tweets_raw(client: tweepy.Client, arg: str):
     # Search Recent Tweets
 
     # This endpoint/method returns Tweets from the last seven days
 
-    response = client.search_recent_tweets(arg, max_results=100)
-    # The method returns a Response object, a named tuple with data, includes,
-    # errors, and meta fields
-    print(response.meta)
-
+    response = client.search_recent_tweets(
+        f"{arg} -is:retweet -is:reply lang:en",
+        max_results=100,
+        tweet_fields=['created_at', "public_metrics", 'text', 'source', 'lang'],
+    )
     # In this case, the data field of the Response returned is a list of Tweet
     # objects
     tweets = response.data
-
     # Each Tweet object has default ID and text fields
-    for tweet in tweets:
-        print(tweet.id)
-        print(tweet.text)
+    return [tweet.data for tweet in tweets]
+
+
+def save_data_to_file():
+    pass
 
 
 def main():
     # You can authenticate as your app with just your bearer token
     client = tweepy.Client(bearer_token=bearer_token)
-    search_tweets_by(client, "Apache Kafka")
+    tweets = extract_recent_tweets_raw(client, "(@apachekafka OR @ApacheAirflow OR @cassandra OR @apachesuperset)")
+    print(tweets[0:2])
 
 
 if __name__ == "__main__":
